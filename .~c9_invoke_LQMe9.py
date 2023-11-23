af3ebd792b3c0be22ef73037f1baf1513273d516
MasashiSato <mssst1116@gmail.com>
2023-11-23 05:39:37 +0000

    add langchain

---
+++ .~c9_invoke_LQMe9.py
@@ -0,0 +1,152 @@
+import os
+
+import streamlit as st
+st.title('My First Streamlit App')
+
+# from dotenv import load_dotenv
+# from langchain.agents import AgentType, initialize_agent, load_tools
+# from langchain.callbacks import StreamlitCallbackHandler
+# from langchain.chat_models import ChatOpenAI
+# from langchain.memory import ConversationBufferMemory
+# from langchain.prompts import MessagesPlaceholder
+
+# load_dotenv()
+
+# def create_agent_chain():
+#     chat = ChatOpenAI(
+#         model_name = os.environ["OPENAI_API_MODEL"],
+#         temperature= os.environ["OPENAI_API_TEMPERATURE"],
+#         streaming=True,
+#     )
+    
+#     agent_kwargs = {
+#         "extra_prompt_messages": [MessagesPlaceholder(variable_name = "memory")],
+#     }
+#     memory = ConversationBufferMemory(memory_key="memory", return_messages=True)
+    
+#     tools = load_tools(["ddg-search", "wikipedia"])
+#     return initialize_agent(
+#         tools,
+#         chat,
+#         agent=AgentType.OPENAI_FUNCTIONS,
+#         agent_kwards=agent_kwards,
+#         memory=memory,
+#     )
+
+# if "agent_chain" not in st.session_state:
+#     st.session_state_chain = create_agent_chain()
+
+# st.title("langchain-streamlit-app")
+
+# if "messages" not in st.session_state:
+#     st.session_state.mesasges = []
+
+
+# for message in st.session_state.messages:
+#     with st.chat_message(message["role"]):
+#         st.markdown(message["content"])
+
+# prompt = st.chat_input("What is up?")
+
+# if prompt:
+#     st.session_state.messages.append({"role": "user", "content": prompt})
+    
+#     with st.chat_message("user"):
+#         st.markdown(prompt)
+    
+#     with st.chat_message("assistant"):
+#         callback = StreamlitCallbackHandler(st.container())
+#         response = st.session_state.agent.run(prompt, callbacks=[callback])
+#         st.markdown(response)
+        
+#     stsession_state.messages.append({"role": "assistant", "content": response})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
